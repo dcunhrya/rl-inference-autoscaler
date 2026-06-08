1a30a490c0593234baa79ff26185fde7b3d56fa7
Ryan DCunha <rdcunha@stanford.edu>
2026-06-08 13:58:21 -0700

    edit intro

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const site = process.env.SITE_URL;
 let base = process.env.BASE_PATH ?? '/';
@@ -15,6 +17,8 @@ export default defineConfig({
     mdx({
       syntaxHighlight: 'shiki',
       gfm: true,
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
     }),
     react(),
     tailwind({
